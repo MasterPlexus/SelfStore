9b7b0f75bba84612f5eae84d60641118ab6a7371
dirk <dirkwindhorst@gmail.com>
2021-08-16 22:23:31 +0200

    Update with permissions

--- app/src/main/java/com/masterplexus/selfstore/ui/DownloadFileFromURL.java
+++ app/src/main/java/com/masterplexus/selfstore/ui/DownloadFileFromURL.java
@@ -1,42 +1,34 @@
 package com.masterplexus.selfstore.ui;
 
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.os.Build.VERSION.SDK_INT;
-import static androidx.core.content.ContextCompat.getSystemService;
+
+import static androidx.core.app.ActivityCompat.requestPermissions;
+import static androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale;
 
 import android.Manifest;
-import android.app.DownloadManager;
-import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
+
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import com.masterplexus.selfstore.RegExSnipped;
 import com.masterplexus.selfstore.ui.home.HomeFragment;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class DownloadFileFromURL extends AsyncTask<String, String, String> {
     private Context context;
@@ -48,7 +40,7 @@ public class DownloadFileFromURL extends AsyncTask<String, String, String> {
     protected String doInBackground(String... arg0) {
         try {
             URL url = new URL(arg0[0]);
-            requestPermission();
+            requestPermissionStorage();
             String AppName =  new RegExSnipped().GetSearch(url.getFile().toString(),".*/(.*?.apk)");
             HttpURLConnection c = (HttpURLConnection) url.openConnection();
             c.setRequestMethod("GET");
@@ -73,6 +65,8 @@ public class DownloadFileFromURL extends AsyncTask<String, String, String> {
             }
             fos.close();
             is.close();
+
+            checkInstallPermission();
 
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -103,7 +97,7 @@ public class DownloadFileFromURL extends AsyncTask<String, String, String> {
 
         }
 
-    private boolean checkPermissionStorrage() {
+    private boolean checkPermissionStorage() {
         if (SDK_INT >= Build.VERSION_CODES.R) {
             return Environment.isExternalStorageManager();
         } else {
@@ -111,11 +105,35 @@ public class DownloadFileFromURL extends AsyncTask<String, String, String> {
         }
     }
 
-    private void requestPermission() {
-        if (!checkPermissionStorrage()) {
+    private void requestPermissionStorage() {
+        if (!checkPermissionStorage()) {
 
             Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+            intent.setData(uri);
+            context.startActivity(intent);
+        }
+    }
+
+    private void checkInstallPermission() {
+        if (ContextCompat.checkSelfPermission(
+                context, Manifest.permission.INSTALL_PACKAGES) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+            Log.i ("Download","access ok");
+        //} else if (shouldShowRequestPermissionRationale()){
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            //showInContextUI(...);
+        } else{
+            Log.i ("Download","starting get request");
+            Intent intent = new Intent();
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setAction(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
             Uri uri = Uri.fromParts("package", context.getPackageName(), null);
             intent.setData(uri);
             context.startActivity(intent);
